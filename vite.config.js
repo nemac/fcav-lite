14bc095d334ab903fb253d4a027df58ae58261e3
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-03-04 10:10:07 -0500

    update base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-base: '/fcav-lite'
 export default defineConfig({
+  base: 'nemac.github.io/fcav-lite',
   plugins: [react()],
 })
