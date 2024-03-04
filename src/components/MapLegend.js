58d7c654219ec5be8ab1ce0fb99ff28e620efe40
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-03-04 10:00:11 -0500

    starting over with vite

--- src/components/MapLegend.js
+++
@@ -1,7 +0,0 @@
-import forwarn2Legend from '../forwarn2-legend.png';
-
-export const MapLegend = () => {
-    return (<div>
-        <img src={forwarn2Legend} margin-right='20%' width='128.5px' height='210.5px' />
-    </div>);
-};
